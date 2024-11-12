2a0362eb5b28654db62fa2ea5181ee2d1e885324
2shaaaa <pttu1704@gmail.com>
2024-11-12 18:07:26 +0700

    add AddBook files

---
+++ src/main/java/com/example/demo/AddBookController.java
@@ -0,0 +1,38 @@
+package com.example.demo;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
+public class AddBookController {
+
+    @FXML
+    private TextField AuthorLabel;
+
+    @FXML
+    private TextField BookIDLabel;
+
+    @FXML
+    private TextField BookTitleLabel;
+
+    @FXML
+    private Button CancelAddBookButton;
+
+    @FXML
+    private TextField PublisherLabel;
+
+    @FXML
+    private Button SaveAddBookButton;
+
+    @FXML
+    void CancelBookAction(MouseEvent event) {
+
+    }
+
+    @FXML
+    void SaveBookAction(MouseEvent event) {
+
+    }
+
+}
