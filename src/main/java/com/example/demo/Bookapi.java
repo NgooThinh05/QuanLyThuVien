2ec82f661eef7791777d529152b48dc11eb2491b
NgooThinh05 <23020574@vnu.edu.vn>
2024-11-17 21:11:59 +0700

    4

--- src/main/java/com/example/demo/Bookapi.java
+++ src/main/java/com/example/demo/Bookapi.java
@@ -5,15 +5,16 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
 
 
 import java.io.File;
 import java.io.IOException;
-
 
 
 public class Bookapi extends Node {
@@ -26,33 +27,33 @@ public class Bookapi extends Node {
 
     @FXML
     private Label namebook;
-    private Book currentBook;
+    private Book book;
 
     @FXML
     private Button chitiet;
 
 
-   public void setData(Book book) {
-       this.currentBook = book;
-       namebook.setText(book.getTitle());
-       author.setText(book.getAuthor());
+    public void setData(Book book) {
+        this.book = book;
+        namebook.setText(book.getTitle());
+        author.setText(book.getAuthor());
 
-       String imagePath = book.getImage();
-       try {
-           if (imagePath != null && !imagePath.isEmpty()) {
-               imageview.setImage(new Image(imagePath));
-           } else {
-               imageview.setImage(new Image(getClass().getResourceAsStream("/Image/logo.png")));
-           }
-       } catch (Exception e) {
-           imageview.setImage(new Image(getClass().getResourceAsStream("/Image/logo.png")));
-           System.out.println("Error loading image: " + e.getMessage());
-       }
+        String imagePath = book.getImage();
+        try {
+            if (imagePath != null && !imagePath.isEmpty()) {
+                imageview.setImage(new Image(imagePath));
+            } else {
+                imageview.setImage(new Image(getClass().getResourceAsStream("/Image/logo.png")));
+            }
+        } catch (Exception e) {
+            imageview.setImage(new Image(getClass().getResourceAsStream("/Image/logo.png")));
+            System.out.println("Error loading image: " + e.getMessage());
+        }
     }
 
 
     public void setDataAll(Book book) {
-        this.currentBook = book;
+        this.book = book;
         namebook.setText(book.getTitle());
         author.setText(book.getAuthor());
         String imagePath = book.getImage();
@@ -65,6 +66,22 @@ public class Bookapi extends Node {
         } catch (Exception e) {
             imageview.setImage(new Image(getClass().getResourceAsStream("/Image/logo.png")));
             System.out.println("Error loading image: " + e.getMessage());
+        }
+    }
+
+    public void chitiet(ActionEvent actionEvent) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("bookdetail.fxml"));
+            Parent root = loader.load();
+            Bookdetail controller = loader.getController();
+            controller.bookdetail(book);
+            Scene scene = new Scene(root);
+            Stage stage = new Stage();
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            System.err.println("Error loading detailed view: " + e.getMessage());
+            e.printStackTrace();
         }
     }
 
---
+++ src/main/java/com/example/demo/Bookdetail.java
@@ -0,0 +1,66 @@
+package com.example.demo;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.PasswordField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class Bookdetail {
+    private Book book;
+    @FXML
+    private AnchorPane anchorpanedetail;
+    @FXML
+    private Text title;
+    @FXML
+    private Text author;
+    @FXML
+    private Text nxb;
+    @FXML
+    private Text describe;
+    @FXML
+    private ImageView imagebook;
+    @FXML
+    private Button back;
+    @FXML
+    private Text theloai;
+
+
+    public void bookdetail(Book book) {
+        this.book = book;
+        title.setText(book.getTitle());
+        author.setText("Author: " + book.getAuthor());
+        nxb.setText("Publisher: " + book.getPublisher());
+        theloai.setText("Category: " + book.getTheloai());
+
+        describe.setText(book.getMota());
+
+        String imagePath = book.getImage();
+        try {
+            if (imagePath != null && !imagePath.isEmpty()) {
+                imagebook.setImage(new Image(imagePath));
+            } else {
+                imagebook.setImage(new Image(getClass().getResourceAsStream("/Image/logo.png")));
+            }
+        } catch (Exception e) {
+            imagebook.setImage(new Image(getClass().getResourceAsStream("/Image/logo.png")));
+            System.out.println("Error loading image: " + e.getMessage());
+        }
+    }
+
+
+
+
+}
