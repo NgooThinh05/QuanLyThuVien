6b449d23b0110b5124e3aa8a89716feef966112e
minhphamcong <162276230+minhphamcong@users.noreply.github.com>
2024-11-28 09:05:59 +0700

    Update signup

--- src/main/java/com/example/demo/signup.java
+++ src/main/java/com/example/demo/signup.java
@@ -6,66 +6,86 @@ import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
 import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
-
-
 import javafx.event.ActionEvent;
 import javafx.stage.StageStyle;
 
-import java.io.File;
 import java.net.URL;
+import java.util.ResourceBundle;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ResourceBundle;
 
 public class signup implements Initializable {
+
     @FXML
     private ImageView signupimage;
+
     @FXML
     private Button closebutton;
+
     @FXML
     private Label signupmessage;
+
     @FXML
     private TextField usernameTextField;
+
     @FXML
     private PasswordField setpasswordfield;
+
     @FXML
     private PasswordField comfirmpasswordfield;
+
     @FXML
     private TextField MSV;
+
     @FXML
     private TextField HoTen;
+
     @FXML
     private TextField sodt;
+
     @FXML
     private TextField diachi;
 
-
-
+    @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        File brandingFile = new File("image/bgrlogin.jpg");
-        Image brandingImage = new Image(brandingFile.toURI().toString());
-        signupimage.setImage(brandingImage);
-    }
-
-    public void SignupButtonAction(ActionEvent actionEvent) throws SQLException {
-        if (!setpasswordfield.getText().equals(comfirmpasswordfield.getText())) {
-            signupmessage.setText("password not match!");
+        // Load the background image for branding
+        try {
+            URL imageUrl = getClass().getResource("/image/");
+            if (imageUrl != null) {
+                Image brandingImage = new Image(imageUrl.toExternalForm());
+                signupimage.setImage(brandingImage);
+            } else {
+                System.out.println("Background image not found. Check the file path!");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        signupuser();
     }
 
-    public void signupuser() throws SQLException {
+    @FXML
+    public void SignupButtonAction(ActionEvent actionEvent) {
+        if (!setpasswordfield.getText().equals(comfirmpasswordfield.getText())) {
+            signupmessage.setText("Passwords do not match!");
+        } else {
+            try {
+                signupuser();
+            } catch (SQLException e) {
+                e.printStackTrace();
+                signupmessage.setText("An error occurred during signup!");
+            }
+        }
+    }
+
+    private void signupuser() throws SQLException {
         DatabaseConnection connection = new DatabaseConnection();
         Connection connectDB = connection.getConnection();
 
-        // Lấy giá trị từ các trường nhập liệu
         String username = usernameTextField.getText();
         String password = setpasswordfield.getText();
         String hoTenText = HoTen.getText();
@@ -73,11 +93,9 @@ public class signup implements Initializable {
         String MSVText = MSV.getText();
         String DiaChi = diachi.getText();
 
-        // Câu lệnh SQL với các placeholder (?)
         String insertQuery = "INSERT INTO users (HoTen, username, password, sodt, CCCD, Diachi) VALUES (?, ?, ?, ?, ?, ?)";
 
         try (PreparedStatement preparedStatement = connectDB.prepareStatement(insertQuery)) {
-            // Gán giá trị cho các placeholder
             preparedStatement.setString(1, hoTenText);
             preparedStatement.setString(2, username);
             preparedStatement.setString(3, password);
@@ -85,37 +103,32 @@ public class signup implements Initializable {
             preparedStatement.setString(5, MSVText);
             preparedStatement.setString(6, DiaChi);
 
-            // Thực thi câu lệnh
             int rowsInserted = preparedStatement.executeUpdate();
             if (rowsInserted > 0) {
                 signupmessage.setText("Signup successful!");
             } else {
                 signupmessage.setText("Signup failed!");
             }
-        } catch (SQLException e) {
-            e.printStackTrace();
-            signupmessage.setText("An error occurred!");
         }
     }
 
-    public void Login(){
-        try {
-            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Login.fxml"));
-            Scene scene = new Scene(fxmlLoader.load(), 1200, 720);
-            Stage stage1 = new Stage();
-            stage1.initStyle(StageStyle.UNDECORATED);
-
-            stage1.setScene(scene);
-            stage1.show();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-
+    @FXML
     public void closeButton(ActionEvent actionEvent) {
         Stage stage = (Stage) closebutton.getScene().getWindow();
         stage.close();
         Login();
+    }
+
+    private void Login() {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Login.fxml"));
+            Scene scene = new Scene(fxmlLoader.load(), 700, 500);
+            Stage stage = new Stage();
+            stage.initStyle(StageStyle.UNDECORATED);
+            stage.setScene(scene);
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
