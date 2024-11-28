5f3ce908087649ec83bc80a1b64e416962e2e4aa
minhphamcong <162276230+minhphamcong@users.noreply.github.com>
2024-11-28 09:07:19 +0700

    sua kick thuoc login

--- src/main/java/com/example/demo/Main.java
+++ src/main/java/com/example/demo/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Login.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1200, 720);
+        Scene scene = new Scene(fxmlLoader.load(), 700, 500);
         stage.initStyle(StageStyle.UNDECORATED);
         stage.setScene(scene);
         stage.show();
